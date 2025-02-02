cec7f7517a8d5d48f52e89eeee46063d95baa1fe
Oscar Franco <ospfranco@gmail.com>
2025-02-02 15:53:11 +0100

    Android loads but dylibs are not correctly loaded

---
+++ android/src/main/java/com/opawslc/JNIOnLoad.java
@@ -0,0 +1,22 @@
+package com.opawslc;
+
+import android.util.Log;
+
+public class JNIOnLoad {
+  private static final String TAG = "OPAwsLC";
+  private static boolean isInitialized = false;
+
+  public synchronized static void initializeNative() {
+    if (isInitialized) return;
+    try {
+      Log.i(TAG, "Loading NitroModules C++ library...");
+      System.loadLibrary("aws_lc_fips_0_13_1_crypto");
+      System.loadLibrary("aws_lc_fips_0_13_1_rust_wrapper");
+      System.loadLibrary("opawslc");
+      isInitialized = true;
+    } catch (Throwable e) {
+      Log.e(TAG, "Failed to load libaws deps.", e);
+      throw e;
+    }
+  }
+}
--- android/src/main/java/com/opawslc/OpAwsLcModule.kt
+++ android/src/main/java/com/opawslc/OpAwsLcModule.kt
@@ -1,9 +1,16 @@
 package com.opawslc
 
+import androidx.annotation.Keep
+import com.facebook.proguard.annotations.DoNotStrip
 import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.common.annotations.FrameworkAPI
 import com.facebook.react.module.annotations.ReactModule
+import com.facebook.react.turbomodule.core.CallInvokerHolderImpl
 
-@ReactModule(name = OpAwsLcModule.NAME)
+@DoNotStrip
+@Keep
+@OptIn(FrameworkAPI::class)
+@Suppress("KotlinJniMissingFunction")
 class OpAwsLcModule(reactContext: ReactApplicationContext) :
   NativeOpAwsLcSpec(reactContext) {
 
@@ -11,13 +18,33 @@ class OpAwsLcModule(reactContext: ReactApplicationContext) :
     return NAME
   }
 
-  // Example method
-  // See https://reactnative.dev/docs/native-modules-android
-  override fun multiply(a: Double, b: Double): Double {
-    return a * b
+  override fun install(): String? {
+    try {
+      // 1. Get jsi::Runtime pointer
+      val jsContext = reactApplicationContext.javaScriptContextHolder
+        ?: return "ReactApplicationContext.javaScriptContextHolder is null!"
+
+      // 2. Get CallInvokerHolder
+      val callInvokerHolder = reactApplicationContext.jsCallInvokerHolder as? CallInvokerHolderImpl
+        ?: return "ReactApplicationContext.jsCallInvokerHolder is null!"
+
+      // 3. Install Nitro
+      install(jsContext.get(), callInvokerHolder)
+
+      return null
+    } catch (e: Throwable) {
+      // ?. Something went wrong! Maybe a JNI error?
+      return e.message
+    }
   }
+
+  private external fun install(jsRuntimePointer: Long, callInvokerHolder: CallInvokerHolderImpl)
 
   companion object {
     const val NAME = "OpAwsLc"
+
+    init {
+      JNIOnLoad.initializeNative()
+    }
   }
 }
