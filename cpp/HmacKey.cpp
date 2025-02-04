fc508f92ffbc91fb72afc50efbd750770e1f49f6
Oscar Franco <ospfranco@gmail.com>
2025-02-04 12:24:32 +0100

    Adjust namespaces

--- cpp/HmacKey.cpp
+++ cpp/HmacKey.cpp
@@ -1,11 +1,11 @@
 #include "HmacKey.h"
-#include "librn_aws_lc.h"
+#include "libaws_lc.h"
 
 namespace opawslc {
 namespace jsi = facebook::jsi;
 
 HmacKey::HmacKey(jsi::Runtime &rt) {
-  int status = awsrc::generate_hmac_sha256_key(&aws_lc_hmac_key);
+  int status = aws_lc::generate_hmac_sha256_key(&aws_lc_hmac_key);
   if (status != 0) {
     throw std::runtime_error("Failed to generate HMAC key");
   }
